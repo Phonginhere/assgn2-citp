b5c141ecc7e44087e9c1cd2dfe544d4742671016
(Phong)Hai Phong Tran <tranhaiphong2016fpt@gmail.com>
2024-07-28 16:57:24 +1000

    Update settings.php

--- assign2/settings.php
+++ assign2/settings.php
@@ -1,9 +1,9 @@
 <?php
 // Database settings
 $host = 'feenix-mariadb.swin.edu.au';
-$dbname = 's104334842_db';
-$user = 's104334842';
-$pass = '@taoXincu17';
+$dbname = '';
+$user = '';
+$pass = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
